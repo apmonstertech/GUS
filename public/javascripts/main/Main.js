3fac201a825a36f5c2dba002f8fed46a7c139b50
Patryk Strzygocki <patryk53@o2.pl>
2019-05-16 01:00:54 +0100

    SVG map, panel and starter

---
+++ public/javascripts/main/Main.js
@@ -0,0 +1,40 @@
+$("#log").click(function () {
+    $(".login-modal").css("display", "block")
+})
+
+$(".cls-15").click(function (e) {
+    var height = $("body")[0].clientHeight
+    var am = 0;
+    $("#up").css("opacity", 0)
+    var count = 0
+    var inter = setInterval(function () {
+        if (height - am > 4) {
+            window.scroll(0, am);
+            am += 4
+            count += 0.009
+            $("#up").css("opacity", count)
+        } else {
+            window.scroll(0, height)
+            clearInterval(inter)
+
+        }
+    }, 1)
+    $("#country_name").html(e.target.id)
+})
+$("#up").click(function () {
+    var height = $("body")[0].clientHeight
+    var am = Math.floor(window.scrollY);
+    var count = 1
+    var inter = setInterval(function () {
+        if (am > 4) {
+            window.scroll(0, am);
+            am -= 4
+            count -= 0.009
+        } else {
+            window.scroll(0, 0);
+            clearInterval(inter)
+        }
+        $("#up").css("opacity", count)
+    }, 1)
+
+})
