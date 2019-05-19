3c707549e13d43e86ae51a20271b2467d0f92af4
xarchie25 <gartur25@o2.pl>
2019-05-19 21:28:58 +0200

    Navbar

--- public/javascripts/forms/NetForm.js
+++ public/javascripts/forms/NetForm.js
@@ -1,25 +1,25 @@
 class NetForm {
     constructor() { }
-    sendDataRegister(username, pass, conPass, email, age) {
-        var obj
-        $.ajax({
-            url: "/users/register/",
-            data: {
-                username: username,
-                password: pass,
-                passwordAgain: conPass,
-                email: email,
-                age: age
-            },
-            type: "POST",
-            success: function (data) {
+    // sendDataRegister(username, pass, conPass, email, age) {
+    //     var obj
+    //     $.ajax({
+    //         url: "/users/register/",
+    //         data: {
+    //             username: username,
+    //             password: pass,
+    //             passwordAgain: conPass,
+    //             email: email,
+    //             age: age
+    //         },
+    //         type: "POST",
+    //         success: function (data) {
 
-            },
-            error: function (xhr, status, error) {
-                console.log(xhr);
-            }
-        });
-    }
+    //         },
+    //         error: function (xhr, status, error) {
+    //             console.log(xhr);
+    //         }
+    //     });
+    // }
     sendDataLogin(username, pass) {
         var obj
         $.ajax({
